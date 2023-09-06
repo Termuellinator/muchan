233c475b210d576229911cae629b1b7357c26604
Termuellinator <saldorin@web.de>
2023-09-06 14:36:46 +0200

    set up basic url, view and template config

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+    # cnt = Resources.objects.all().count()
+    # user_cnt = User.objects.filter(is_active=True).count()
+    # res_per_cat = Resources.objects.values("cat_id__cat").annotate(cnt=Count("cat_id"))
+    
+    context = {
+        # "cnt": cnt,
+        # "user_cnt": user_cnt,
+        # "res_per_cat": res_per_cat
+    }
+    return render(
+        request=request, 
+        template_name="index.html", 
+        context=context
+    )
---
+++ apps/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.user import views
+
+urlpatterns = [
+    #path("", views.home_page, name='home'),
+    #path("resource/<int:id>", views.resource_detail, name="resource-detail")
+]
