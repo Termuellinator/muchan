6ea9c3cc9b410ca0bf6e73f0696793d3b5c32fe8
Termuellinator <saldorin@web.de>
2023-09-13 11:36:36 +0200

    Convert the post views to class based views

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.post import views
 
 urlpatterns = [
-    path("", views.home_page, name='home'),
-    path("post/<int:id>", views.post_page, name="post-detail"),
-    path("post/new_post", views.new_post, name="new-post")
+    path("", views.HomePageView.as_view(), name='home'),
+    path("post/<int:post_id>", views.PostPageView.as_view(), name="post-detail"),
+    path("post/new_post", views.NewPostView.as_view(), name="new-post")
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -1,116 +1,122 @@
 from django.shortcuts import render, redirect
+from django.views import View
 
 from .models import Post, Comment, PostTag, Category, Tag
 from apps.user.models import User
 from .form import NewComment, NewPost
+
+
 # Create your views here.
 
-def home_page(request):
+class HomePageView(View):
     """Home Page with the 10 most recent posts displayed
     """
-    posts = (Post.objects
+    template_name = "index.html"
+    
+    def get(self, request):
+        posts = (Post.objects
             .select_related("user_id", "cat_id")
             .prefetch_related("tags")
             .all()
             .order_by('-created_at')[:10])
 
-    context = {
-        "posts": posts
-    }
-    return render(
-        request=request, 
-        template_name="index.html", 
-        context=context
-    )
+        context = {
+            "posts": posts
+        }
+        return render(
+            request=request, 
+            template_name=self.template_name, 
+            context=context
+        )
 
-def post_page(request, id):
+class PostPageView(View):
     """Display the detailed page of a specific post.
     
     Offers the possibility to post a comment for that post.
-
-    Args:
-        id (int): the id of the post to be displayed
     """
-    post = (Post.objects
-            .select_related("user_id", "cat_id")
-            .prefetch_related("tags")
-            .get(pk=id))
-    comments = (Comment.objects
-                .select_related("user_id")
-                .all()
-                .filter(post_id = id)
-                .order_by('-created_at'))
-    
-    context = {
-        "title": post.title,
-        "username": post.user_id.username,
-        "image": post.image.url,
-        "upvotes": post.userUpVotes.count(),
-        "downvotes": post.userDownVotes.count(),
-        "category": post.cat_id.cat,
-        "tags": ", ".join([tag.name for tag in post.tags.all()]),
-        "comments": comments
-    }
-    
-    if request.method == "GET":
+
+    template_name = "post/post.html"
+
+    def get(self, request, post_id):
+
+        self.post_data = (Post.objects
+                .select_related("user_id", "cat_id")
+                .prefetch_related("tags")
+                .get(pk=post_id))
+        self.comments = (Comment.objects
+                    .select_related("user_id")
+                    .all()
+                    .filter(post_id = post_id)
+                    .order_by('-created_at'))
+
+        self.context = {
+            "title": self.post_data.title,
+            "username": self.post_data.user_id.username,
+            "image": self.post_data.image.url,
+            "upvotes": self.post_data.userUpVotes.count(),
+            "downvotes": self.post_data.userDownVotes.count(),
+            "category": self.post_data.cat_id.cat,
+            "tags": ", ".join([tag.name for tag in self.post_data.tags.all()]),
+            "comments": self.comments
+        }
         form = NewComment()
-        context["form"] = form
+        self.context["form"] = form
         return render(
             request=request, 
-            template_name="post/post.html", 
-            context=context
+            template_name=self.template_name, 
+            context=self.context
         )
-    else:
+        
+    def post(self, request, post_id):
         form = NewComment(request.POST)
         if form.is_valid():
             data = form.cleaned_data
             new_comment = Comment.objects.create(
                 user_id = User.objects.get(pk=data['user_id']),
-                post_id = post,
+                post_id = Post.objects.get(pk=post_id),
                 body = data["body"]
             )
-            form = NewComment()
-            context["form"] = form
-            return render(
-                request=request, 
-                template_name="post/post.html", 
-                context=context
-            )
+            return redirect('post-detail', post_id)
         return render(
             request=request, 
-            template_name="post/post.html", 
-            context=context
+            template_name=self.template_name, 
+            context=self.context
         )
         
-def new_post(request):
+
+class NewPostView(View):
     """Display the page to create a new post.
     On successful creation, redirects to the detail page for that post.
     """
-    if request.method == "GET":
+    template_name = "post/new_post.html"
+    
+    def get(self, request):
         form = NewPost()
         return render(
             request=request,
-            template_name="post/new_post.html",
+            template_name=self.template_name,
             context={'form': form},
         )
-    else:
+    
+    def post(self, request):
         form = NewPost(request.POST, request.FILES)
-        print(form)
         if form.is_valid():
             data = form.cleaned_data
-            print(data)            
             created_post = Post.objects.create(
-                user_id = User.objects.get(pk=data['user_id']), 
-                title = data['title'], 
-                image = data.get('image'),
-                cat_id = Category.objects.get(pk=data['cat_id']))
+                user_id=User.objects.get(pk=data['user_id']), 
+                title=data['title'], 
+                image=data.get('image'),
+                cat_id=Category.objects.get(pk=data['cat_id'])
+            )
             created_post.save()
             for tag in data["tags"]:
-                PostTag.objects.create(post_id=created_post, 
-                                            tag_id=Tag.objects.get(pk=tag))
+                PostTag.objects.create(
+                    post_id=created_post, 
+                    tag_id=Tag.objects.get(pk=tag)
+                )
             return redirect('post-detail', created_post.id)
         return render(
             request=request,
-            template_name="post/new_post.html",
+            template_name=self.template_name,
             context={'form': form},
         )
