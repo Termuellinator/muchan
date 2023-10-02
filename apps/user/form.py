c2bdf1aa7fac69120fb252a1f68020cc355d4ec7
Termuellinator <saldorin@web.de>
2023-10-02 13:41:48 +0200

    Add unit tests for user.form.RegisterUserForm

--- apps/user/form.py
+++ apps/user/form.py
@@ -3,14 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 
 from .models import User
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
+
 
 class RegisterUserForm(UserCreationForm):    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        
+
+
 class ModifyUserForm(ModelForm):
     class Meta:
         model = User
--- apps/user/tests/ut/test_forms.py
+++ apps/user/tests/ut/test_forms.py
@@ -1,40 +1,39 @@
 from django.test import TestCase
 
-from apps.user.form import UserCreationForm, ModifyUserForm
+from apps.user.form import RegisterUserForm, ModifyUserForm
 
 
-# class TestUserCreationForm(TestCase):
-#     def setUp(self):
-#         self.username = "Testuser"
-#         self.email = "Test@user.com"
-#         self.password = "Testpassword@2023"
+class TestRegisterUserForm(TestCase):
+    def setUp(self):
+        self.username = "Testuser"
+        self.email = "Test@user.com"
+        self.password = "Testpassword@2023"
 
-#     def test_form_is_valid_for_good_data(self):
-#         data = {
-#             "username": self.username,
-#             "email": self.email,
-#             "password1": self.password,
-#             "password2": self.password
-#         }
-        
-#         form = UserCreationForm(data=data)
-        
-#         self.assertTrue(form.is_valid())
-        
-#     def test_form_not_valid_with_different_pw(self):
-#         data = {
-#             "username": self.username,
-#             "email": self.email,
-#             "password1": self.password,
-#             "password2": self.password
-#         }
-        
-#         form = UserCreationForm(data=data)
-#         # breakpoint()
-#         self.assertTrue(form.is_valid())
-#         self.assertEqual(form["password2"].errors, 
-#                          [str(form.error_messages['password_mismatch'])])
-        
+    def test_form_is_valid_for_good_data(self):
+        data = {
+            "username": self.username,
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password
+        }
+
+        form = RegisterUserForm(data=data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_form_not_valid_with_different_pw(self):
+        data = {
+            "username": self.username,
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password + "123"
+        }
+
+        form = RegisterUserForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form["password2"].errors,
+                         [str(form.error_messages['password_mismatch'])])
+
 
 class TestModifyUserForm(TestCase):
     def setUp(self):
@@ -43,7 +42,7 @@ class TestModifyUserForm(TestCase):
         self.email = "Test@user.com"
         self.title = "Tester"
         self.bio = "Testing is my destiny!"
-        
+
     def test_form_is_valid_for_good_data(self):
         data = {
             "first_name": self.first_name,
@@ -52,7 +51,7 @@ class TestModifyUserForm(TestCase):
             "title": self.title,
             "bio": self.bio
         }
-        
+
         form = ModifyUserForm(data=data)
-        
+
         self.assertTrue(form.is_valid())
