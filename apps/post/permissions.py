dd5f92b7dcaced9fa820e86ef16afed24e7bd726
Termuellinator <saldorin@web.de>
2023-10-07 15:21:48 +0200

    Add Docstrings and extend scope of IsAuthenticatedCreateOrSuperDeleteReadonly to PUT and PATCH

--- apps/post/permissions.py
+++ apps/post/permissions.py
@@ -2,36 +2,44 @@ from rest_framework import permissions
 
 
 class AuthorSuperOrReadOnly(permissions.BasePermission):
+    """Allows only the author or a superuser to modify.
+    SAFE_METHODS are allowed for authenticated users.
+    """
+
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
         return False
-        
+
     def has_object_permission(self, request, view, obj):
         if request.user.is_superuser:
             return True
-        
+
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         if request.user.id == obj.user_id.id:
             return True
-        
+
         return False
 
 
 class IsAuthenticatedCreateOrSuperDeleteOrReadOnly(permissions.BasePermission):
+    """Allows authenticated users to create, superusers to modify and delete.
+    Unauthenticated users can only use SAFE_METHODS.
+    """
+
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         if request.user.is_authenticated:
             if request.user.is_superuser:
                 return True
 
-            if request.method == "DELETE":
+            if request.method in ("DELETE", "PUT", "PATCH"):
                 return False
-            
+
             return True
-        
+
         return False
