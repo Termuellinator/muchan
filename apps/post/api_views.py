9ab85b5db8882942a7d2f76dcc5da3aac9f23ba2
Termuellinator <saldorin@web.de>
2023-10-09 19:26:02 +0200

    Add API endpoint to Up- and Downvote Posts and Comments

--- apps/post/api_views.py
+++ apps/post/api_views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from apps.post import models, serializers, mixins, permissions
 
@@ -37,3 +41,71 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = (models.Comment.objects.all())
 
     serializer_class = serializers.CommentModelSerializer
+
+
+class UpVotePostView(APIView):
+    """Upvote a post for an authenticated user
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, post_id):
+        try:
+            self.post = models.Post.objects.get(pk=post_id)
+            self.post.userUpVotes.add(request.user)
+            self.post.userDownVotes.remove(request.user)
+
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response({"error": "Post not found"},
+                status=status.HTTP_404_NOT_FOUND)
+
+
+class DownVotePostView(APIView):
+    """Downvote a post for an authenticated user
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, post_id):
+        try:
+            self.post = models.Post.objects.get(pk=post_id)
+            self.post.userDownVotes.add(request.user)
+            self.post.userUpVotes.remove(request.user)
+
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response({"error": "Post not found"},
+                status=status.HTTP_404_NOT_FOUND)
+
+
+class UpVoteCommentView(APIView):
+    """Upvote a comment for an authenticated user
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, comment_id):
+        try:
+            self.comment = models.Comment.objects.get(pk=comment_id)
+            self.comment.userUpVotes.add(request.user)
+            self.comment.userDownVotes.remove(request.user)
+
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response({"error": "Comment not found"},
+                status=status.HTTP_404_NOT_FOUND)
+
+
+class DownVoteCommentView(APIView):
+    """Downvote a comment for an authenticated user
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, comment_id):
+        try:
+            self.comment = models.Comment.objects.get(pk=comment_id)
+            self.comment.userDownVotes.add(request.user)
+            self.comment.userUpVotes.remove(request.user)
+
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response({"error": "Comment not found"},
+                status=status.HTTP_404_NOT_FOUND)
--- apps/post/urls.py
+++ apps/post/urls.py
@@ -10,18 +10,54 @@ router.register("api/v1/category", api_views.CategoryViewSet)
 router.register("api/v1/tag", api_views.TagViewSet)
 router.register("api/v1/comment", api_views.CommentViewSet)
 
+api_urlpatterns = [
+    path(
+        "api/v1/upvote-post/<int:post_id>",
+        api_views.UpVotePostView.as_view(),
+        name="upvote-post",
+    ),
+    path(
+        "api/v1/downvote-post/<int:post_id>",
+        api_views.DownVotePostView.as_view(),
+        name="downvote-post",
+    ),
+    path(
+        "api/v1/upvote-comment/<int:comment_id>",
+        views.UpVoteCommentView.as_view(),
+        name="upvote-comment",
+    ),
+    path(
+        "api/v1/downvote-comment/<int:comment_id>",
+        api_views.DownVoteCommentView.as_view(),
+        name="downvote-comment",
+    ),
+]
 
 urlpatterns = [
-    path("", views.HomePageView.as_view(), name='home'),
+    path("", views.HomePageView.as_view(), name="home"),
     path("post/<int:post_id>", views.PostPageView.as_view(), name="post-detail"),
     path("post/new_post", views.NewPostView.as_view(), name="new-post"),
-    path("post/upvote/<int:post_id>", views.UpVotePostView.as_view(),
-         name="post-upvote"),
-    path("post/downvote/<int:post_id>", views.DownVotePostView.as_view(),
-         name="post-downvote"),
-    path("comment/upvote/<int:comment_id>", views.UpVoteCommentView.as_view(),
-         name="comment-upvote"),
-    path("comment/downvote/<int:comment_id>", views.DownVoteCommentView.as_view(),
-         name="comment-downvote"),
+    path(
+        "post/upvote/<int:post_id>", 
+        views.UpVotePostView.as_view(), 
+        name="post-upvote"
+    ),
+    path(
+        "post/downvote/<int:post_id>",
+        views.DownVotePostView.as_view(),
+        name="post-downvote",
+    ),
+    path(
+        "comment/upvote/<int:comment_id>",
+        views.UpVoteCommentView.as_view(),
+        name="comment-upvote",
+    ),
+    path(
+        "comment/downvote/<int:comment_id>",
+        views.DownVoteCommentView.as_view(),
+        name="comment-downvote",
+    ),
+    *api_urlpatterns,
     *router.urls,
 ]
+
